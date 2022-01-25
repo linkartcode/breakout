0f0be93cab64ebc8451287661f1df757da19e35e
Roman Protchev <s1973z@mail.ru>
2022-01-25 22:20:50 +0300

    init

---
+++ main.lua
@@ -0,0 +1,132 @@
+--[[
+    GD50
+    Breakout Remake
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Originally developed by Atari in 1976. An effective evolution of
+    Pong, Breakout ditched the two-player mechanic in favor of a single-
+    player game where the player, still controlling a paddle, was tasked
+    with eliminating a screen full of differently placed bricks of varying
+    values by deflecting a ball back at them.
+
+    This version is built to more closely resemble the NES than
+    the original Pong machines or the Atari 2600 in terms of
+    resolution, though in widescreen (16:9) so it looks nicer on 
+    modern systems.
+
+    Credit for graphics (amazing work!):
+    https://opengameart.org/users/buch
+
+    Credit for music (great loop):
+    http://freesound.org/people/joshuaempyre/sounds/251461/
+    http://www.soundcloud.com/empyreanma
+]]
+
+require 'src/Dependencies'
+
+--[[
+    Called just once at the beginning of the game; used to set up
+    game objects, variables, etc. and prepare the game world.
+]]
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Breakout')
+    math.randomseed(os.time())
+
+    gFonts = {
+        ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+        ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+        ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+    }
+    love.graphics.setFont(gFonts['small'])
+    gTextures = {
+        ['background'] = love.graphics.newImage('graphics/background.png'),
+        ['main'] = love.graphics.newImage('graphics/breakout.png'),
+        ['arrows'] = love.graphics.newImage('graphics/arrows.png'),
+        ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
+        ['particle'] = love.graphics.newImage('graphics/particle.png')
+    }
+    gFrames = {
+        ['arrows'] = GenerateQuads(gTextures['arrows'], 24, 24),
+        ['paddles'] = GenerateQuadsPaddles(gTextures['main']),
+        ['balls'] = GenerateQuadsBalls(gTextures['main']),
+        ['bricks'] = GenerateQuadsBricks(gTextures['main']),
+        ['hearts'] = GenerateQuads(gTextures['hearts'], 10, 9)
+    }
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end,
+        ['serve'] = function() return ServeState() end,
+        ['game-over'] = function() return GameOverState() end,
+        ['victory'] = function() return VictoryState() end,
+        ['high-scores'] = function() return HighScoreState() end,
+        ['enter-high-score'] = function() return EnterHighScoreState() end,
+        ['paddle-select'] = function() return PaddleSelectState() end
+    }
+    gSounds = {
+        ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['confirm'] = love.audio.newSource('sounds/confirm.wav', 'static'),
+        ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+        ['no-select'] = love.audio.newSource('sounds/no-select.wav', 'static'),
+        ['brick-hit-1'] = love.audio.newSource('sounds/brick-hit-1.wav', 'static'),
+        ['brick-hit-2'] = love.audio.newSource('sounds/brick-hit-2.wav', 'static'),
+        ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+        ['victory'] = love.audio.newSource('sounds/victory.wav', 'static'),
+        ['recover'] = love.audio.newSource('sounds/recover.wav', 'static'),
+        ['high-score'] = love.audio.newSource('sounds/high_score.wav', 'static'),
+        ['pause'] = love.audio.newSource('sounds/pause.wav', 'static'),
+        ['music'] = love.audio.newSource('sounds/music.wav', 'static')
+    }
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+    gStateMachine:change('start', {
+        highScores = loadHighScores()
+    })
+    --gSounds['music']:play()
+    --gSounds['music']:setLooping(true)
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.update(dt)
+    gStateMachine:update(dt)
+    love.keyboard.keysPressed = {}
+end
+
+function love.keypressed(key)
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.draw()
+    push:apply('start')
+
+    local backgroundWidth = gTextures['background']:getWidth()
+    local backgroundHeight = gTextures['background']:getHeight()
+    local scaleX = VIRTUAL_WIDTH / (backgroundWidth - 1)
+    local scaleY = VIRTUAL_HEIGHT / (backgroundHeight - 1)
+    love.graphics.draw(gTextures['background'], 0, 0, 0, scaleX, scaleY)
+    
+    gStateMachine:render()
+    displayFPS()
+
+    push:apply('end')
+end
---
+++ src/QuadOperate.lua
@@ -0,0 +1,102 @@
+--[[
+    GD50
+    Breakout Remake
+
+    -- StartState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Functions for Quads operate
+]]
+
+function GenerateQuads(atlas, tilewidth, tileheight)
+    local sheetWidth = atlas:getWidth() / tilewidth
+    local sheetHeight = atlas:getHeight() / tileheight
+
+    local sheetCounter = 1
+    local spritesheet = {}
+
+    for y = 0, sheetHeight - 1 do
+        for x = 0, sheetWidth - 1 do
+            spritesheet[sheetCounter] =
+                love.graphics.newQuad(x * tilewidth, y * tileheight, tilewidth,
+                tileheight, atlas:getDimensions())
+            sheetCounter = sheetCounter + 1
+        end
+    end
+
+    return spritesheet
+end
+
+function table.slice(tbl, first, last, step)
+    local sliced = {}
+  
+    for i = first or 1, last or #tbl, step or 1 do
+      sliced[#sliced+1] = tbl[i]
+    end
+  
+    return sliced
+end
+
+function GenerateQuadsBricks(atlas)
+    return table.slice(GenerateQuads(atlas, 32, 16), 1, 21)
+end
+
+function GenerateQuadsPaddles(atlas)
+    local x = 0
+    local y = 64
+
+    local counter = 1
+    local quads = {}
+
+    for i = 0, 3 do
+        -- smallest
+        quads[counter] = love.graphics.newQuad(x, y, 32, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+        -- medium
+        quads[counter] = love.graphics.newQuad(x + 32, y, 64, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+        -- large
+        quads[counter] = love.graphics.newQuad(x + 96, y, 96, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+        -- huge
+        quads[counter] = love.graphics.newQuad(x, y + 16, 128, 16,
+            atlas:getDimensions())
+        counter = counter + 1
+
+        -- prepare X and Y for the next set of paddles
+        x = 0
+        y = y + 32
+    end
+
+    return quads
+end
+
+function GenerateQuadsBalls(atlas)
+    local x = 96
+    local y = 48
+
+    local counter = 1
+    local quads = {}
+
+    for i = 0, 3 do
+        quads[counter] = love.graphics.newQuad(x, y, 8, 8, atlas:getDimensions())
+        x = x + 8
+        counter = counter + 1
+    end
+
+    x = 96
+    y = 56
+
+    for i = 0, 2 do
+        quads[counter] = love.graphics.newQuad(x, y, 8, 8, atlas:getDimensions())
+        x = x + 8
+        counter = counter + 1
+    end
+
+    return quads
+end
